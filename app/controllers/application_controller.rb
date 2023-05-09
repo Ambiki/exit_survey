3d3df4ed278abfdebd7a6d56d82dec4319394f9b
abeidahmed <abeidahmed92@gmail.com>
2023-05-09 14:06:40 +0530

    add devise like methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  class UnauthorizedError < StandardError; end
+
+  private
+
+  def current_user
+    @current_user ||= User.last
+  end
+
+  def authenticate_user!
+    raise UnauthorizedError unless current_user
+  end
 end
