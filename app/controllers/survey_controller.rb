9ee5ccc37f16cdfafb6385dfec95a77ef236575c
abeidahmed <abeidahmed92@gmail.com>
2023-05-09 14:42:19 +0530

    cleanup user after unsubscribing

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,8 +1,14 @@
 class SurveyController < ApplicationController
   before_action :authenticate_user!
 
+  # If a user re-subscribes within the grace period, we don't want to process the job.
+  GRACE_PERIOD = 1.week
+
   def create
-    current_user.surveys.create!(survey_param)
+    current_user.surveys.create(survey_param)
+    current_user.stop_active_subscriptions
+    SeeYouSoonJob.set(wait: GRACE_PERIOD).perform_later(current_user)
+
     redirect_to see_you_soon_path, notice: "We hope to see you again!"
   end
 
